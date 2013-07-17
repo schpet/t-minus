d985c45a24d8ccc809f1109150815a6894b05453
Peter Schilling <peter@peterschilling.org>
2013-07-17 20:23:26 +0200

    use sessions to keep track of users that have subscribed

--- app/controllers/prelaunch_controller.rb
+++ app/controllers/prelaunch_controller.rb
@@ -2,13 +2,16 @@ class PrelaunchController < ActionController::Base
   layout 'prelaunch'
 
   def new
-    @prelaunch_subscriber = PrelaunchSubscriber.new
+    unless session[:prelaunch_subscriber] == true
+      @prelaunch_subscriber = PrelaunchSubscriber.new
+    end
   end
 
   def create
     @prelaunch_subscriber = PrelaunchSubscriber.new(params.require(:prelaunch_subscriber).permit(:email))
     if @prelaunch_subscriber.save
-      redirect_to root_url, :notice => 'Thank you for your interest. We have sent you an email to confirm your subscription to our prelaunch mailing list.'
+      session[:prelaunch_subscriber] = true
+      redirect_to root_url
     else
       render :action => 'new'
     end
